c4482483b882060f2fac5e0199d35cbb3c81b716
Marin Ivanov <marinsivanov@gmail.com>
2019-10-20 23:04:18 +0300

    Added Selenium Grid structure

--- Homework/Tests/BaseTest.cs
+++ Homework/Tests/BaseTest.cs
@@ -1,6 +1,7 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
 using System;
 using System.IO;
 using System.Reflection;
@@ -19,6 +20,14 @@ namespace Homework
             Driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
             Driver.Manage().Window.Maximize();
             Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
+
+    //        ChromeOptions options = new ChromeOptions();
+
+    //        options.PlatformName = "windows";
+    //        options.BrowserVersion = "77.0";
+
+    //        Driver = new RemoteWebDriver(new Uri("http://192.168.0.101:4444/wd/hub"),options.ToCapabilities(), TimeSpan.FromSeconds(10));
+    //        Driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(30);
         }
 
         [OneTimeTearDown]
